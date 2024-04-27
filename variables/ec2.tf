299abdcd3d911bd80f191c5e9359bf3ed01be10c
karthik <karthikkunchala170.com>
2024-04-27 17:24:20 +0530

    datatypes as variables and refering values from those variables added datatypes.tf

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -5,7 +5,7 @@
 resource "aws_instance" "terraform-automated-ec2" {
   ami = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]  # resource attribute refrence
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]  # resource attribute refrence and list datatype
 
   tags = {
     Name = "ec2-terraform"
