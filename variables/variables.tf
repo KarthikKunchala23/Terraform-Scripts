d110391204cc45dbce0da0424daec644afc825b4
karthik <karthikkunchala170.com>
2024-04-27 16:57:31 +0530

    added ec2.tf file with resource attribute refrence of sg

--- variables/variables.tf
+++ variables/variables.tf
@@ -3,5 +3,6 @@ variable "vpc_ip" {}
 
 variable "ssh_port" {}
 
+
 #if we need to give environment variable value while apply automatically then in windows add environment varibles in key pair ex:TF_VAR_VARIABLENAME also value and other way in cli is terraform plan/apply -var="variable_name=variable_value"
 #for linux export TF_VAR_VARIABLENAME = "VALUE"
