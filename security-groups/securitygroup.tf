a6d2014f05ec776d91d75feb8d4e1cde87942c14
karthik <karthikkunchala170.com>
2024-04-27 12:42:01 +0530

    added variables tf file and modified securitygroup.tf file with variables

--- security-groups/securitygroup.tf
+++ security-groups/securitygroup.tf
@@ -9,10 +9,10 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id  #cross resource attribute refrence
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  cidr_ipv4         = var.vpc_ip
+  from_port         = var.ssh_port
   ip_protocol       = "tcp" #can use udp 
-  to_port           = 80
+  to_port           = var.ssh_port
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
---
+++ security-groups/variables.tf
@@ -0,0 +1,9 @@
+
+
+variable "vpc_ip" {
+  default = "10.203.245.123/32"
+}
+
+variable "ssh_port" {
+  default = "22"
+}
