5bb69a37c995146b45ff18ed24ba6cbeded653ea
karthik <karthikkunchala170.com>
2024-04-27 12:05:21 +0530

    terraform scripts first commit

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "terraform-automated-ec2" {
+  ami = "ami-04e5276ebb8451442"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2-terraform"
+  }
+}
+
+resource "aws_eip" "lb" {
+  domain = "vpc"
+} #elastic ip
