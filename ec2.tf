fac4644fa9f51abe447494439ccea91db2fdc6d1
karthik <karthikkunchala170.com>
2024-04-28 11:58:25 +0530

    added locals in configuration file

--- ec2.tf
+++ ec2.tf
@@ -2,13 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {   #A local value assigns a name to an expression, so you can use the name multiple times within a module instead of repeating the expression.
+  common_tags ={
+    Service = "AppServer"
+    Owner = "DevOps Team"
+  }
+} 
+
 resource "aws_instance" "terraform-automated-ec2" {
   ami = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
+  tags = local.common_tags
 
-  tags = {
-    Name = "ec2-terraform"
-  }
+  # tags = {
+  #   Name = "ec2-terraform"
+  # }
 }
 
 resource "aws_eip" "lb" {
