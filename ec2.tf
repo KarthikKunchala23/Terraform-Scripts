81f47668af6694aa5ff0282bf43bc76d708aa5ea
karthik <karthikkunchala170.com>
2024-04-30 12:50:59 +0530

    added datasource module functions module and load order module edited gitignore

--- ec2.tf
+++ ec2.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "terraform-automated-ec2" {
   ami = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   tags = local.common_tags
+  count = 2
 
   # tags = {
   #   Name = "ec2-terraform"
