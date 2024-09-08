88afad127f7ad840ae8c46029920370c9fc6e590
karthik <karthikkunchala170.com>
2024-09-08 11:39:54 +0530

    added workspace concept with remote backend

--- backendLocking/backendlock.tf
+++ backendLocking/backendlock.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tfstatebackend2024-09-06"
-    key    = "Network/ec2.tfstate"
+    key    = "workspace/ec2.tfstate"
     region = "us-east-1"
     dynamodb_table = "tfstate-lock-table"
   }
--- backendLocking/ec2.tf
+++ backendLocking/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "new" {
   ami = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
+  count = terraform.workspace == "default" ? 2 : 1
 
   tags = {
-    Name = "tfstate file lock ex ec2"
+    Name = "vm-${terraform.workspace}-${count.index}"
     Type = "t2 family"
     Environment = "dev"
   }
